245640d62b95caf92715ec9b79c7fc929e0f8c73
Denis Machard <5562930+dmachard@users.noreply.github.com>
2024-01-12 17:24:46 +0100

    Update monitor.py

--- publicaddr_ovhcloud/monitor.py
+++ publicaddr_ovhcloud/monitor.py
@@ -9,7 +9,7 @@ from publicaddr_ovhcloud import ovhapi
 
 logger = logging.getLogger("monitor")
 
-async def monitor(every, zone, subdomains, ovh_ep, ovh_ak, ovh_as, ovh_ck):
+async def monitor(every, zone, subdomains, ovh_ep, ovh_ak, ovh_as, ovh_ck, has_ipv6):
     cur_ip4 = "127.0.0.1"
     cur_ip6 = "::1"
 
@@ -22,7 +22,10 @@ async def monitor(every, zone, subdomains, ovh_ep, ovh_ak, ovh_as, ovh_ck):
 
     while True:
         try:
-            publicip = publicaddr.lookup()
+            if has_ipv6:
+                publicip = publicaddr.lookup(ip=publicaddr.IPv6)
+            else:
+                publicip = publicaddr.lookup()
             logger.debug("current ip4=%s ip6=%s" % (cur_ip4, cur_ip6))
             logger.debug("your public ip4=%s ip6=%s" % (publicip["ip4"], publicip["ip6"]))
 
@@ -32,11 +35,12 @@ async def monitor(every, zone, subdomains, ovh_ep, ovh_ak, ovh_as, ovh_ck):
                     ovhclient.add_records(zone=zone, subdomains=subdomains, rdata=publicip["ip4"], rtype="A")
                     cur_ip4 = publicip["ip4"]
 
-            if publicip["ip6"] is not None:
-                if publicip["ip6"] != cur_ip6:
-                    logger.debug("updating your public ip6")
-                    ovhclient.add_records(zone=zone, subdomains=subdomains, rdata=publicip["ip6"], rtype="AAAA")
-                    cur_ip6 = publicip["ip6"]
+            if has_ipv6:
+                if publicip["ip6"] is not None:
+                    if publicip["ip6"] != cur_ip6:
+                        logger.debug("updating your public ip6")
+                        ovhclient.add_records(zone=zone, subdomains=subdomains, rdata=publicip["ip6"], rtype="AAAA")
+                        cur_ip6 = publicip["ip6"]
         except Exception as e:
             logger.error("%s" % e)
 
@@ -70,6 +74,10 @@ def start_monitor():
     # enable logger
     setup_logger(debug=debug)
 
+    has_ipv6_env = os.getenv('PUBLICADDR_OVHCLOUD_HAS_IPV6')
+    if has_ipv6_env is not None:
+        has_ipv6 = bool( int(has_ipv6_env) )
+        
     delay_env = os.getenv('PUBLICADDR_OVHCLOUD_UPDATE')
     if delay_env is not None:
         delay_every = int(delay_env)
@@ -112,6 +120,7 @@ def start_monitor():
                             zone=zone, subdomains=subdomains,
                             ovh_ep=ovh_ep, ovh_ak=ovh_ak, 
                             ovh_as=ovh_as, 
-                            ovh_ck=ovh_ck))
+                            ovh_ck=ovh_ck,
+                            has_ipv6=has_ipv6))
     except KeyboardInterrupt:
         logger.debug("exit called")
